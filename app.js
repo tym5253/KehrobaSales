735d8764a58d4923dfbd39f313ad218580f59c06
tym5253 <taha.y.m@gmail.com>
2022-01-29 16:15:34 +0530

    made changes in server app.js

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ const app= express()
 app.use(router);
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
 }
 
 app.listen(PORT,()=>{
